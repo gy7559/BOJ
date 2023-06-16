2527d6b1b935e241bac7d392537236a9fbcb2507
gy7559 <gy7559@naver.com>
2023-06-16 14:26:23 +0900

    update

---
+++ BOJ03.java
@@ -0,0 +1,17 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class BOJ03 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);//입력받는 인스턴스 생성
+        int a = sc.nextInt();//입력받아 정수로 a에 입력
+        int b = sc.nextInt();//입력받아 정수로 b에 입력
+        sc.close();
+        
+        System.out.println(a*b);
+
+	}
+
+}
