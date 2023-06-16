fe7e13a8cd5052b3c336d92014cde5eff176b94a
gy7559 <gy7559@naver.com>
2023-06-16 15:37:17 +0900

    Update BOJ04.java

--- BOJ04.java
+++ BOJ04.java
@@ -6,8 +6,8 @@ public class BOJ04 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);//입력받는 인스턴스 생성
-        int a = sc.nextInt();//입력받아 정수로 a에 입력
-        int b = sc.nextInt();//입력받아 정수로 b에 입력
+        double a = sc.nextDouble();//입력받아 실수로 a에 입력
+        double b = sc.nextDouble();//입력받아 실수로 b에 입력
         sc.close();
         
         System.out.println(a/b);
