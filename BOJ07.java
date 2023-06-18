e59a973f8c2f32847fbd3edf788c023e55714e90
gy7559 <gy7559@naver.com>
2023-06-18 16:27:42 +0900

    Create BOJ07.java

---
+++ BOJ07.java
@@ -0,0 +1,11 @@
+package BOJ;
+import java.util.*;
+public class BOJ07 {
+
+	public static void main(String[] args) {
+		 Scanner scan = new Scanner(System.in);
+	        int a = scan.nextInt();
+	        System.out.println(a-543);
+	}
+
+}
