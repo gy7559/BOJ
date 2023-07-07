2a55a4f18ee0301d97fbb933429e0a8a9c787134
gy7559 <gy7559@naver.com>
2023-07-07 13:38:42 +0900

    Create BOJ23.java

---
+++ BOJ23.java
@@ -0,0 +1,31 @@
+package BOJ;
+import java.io.*;
+import java.util.*;
+
+public class BOJ23 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		String c = bf.readLine();
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int c1 = Integer.parseInt(c);
+		int[] w1 = new int[c1];
+		int[] w2 = new int[c1];
+		for(int a =0; a<c1;a++) {
+			String w = bf.readLine();
+			String[] w4 = w.split(" ");
+			w1[a] = Integer.parseInt(w4[0]);
+			w2[a] = Integer.parseInt(w4[1]);		
+		}
+		int t=0;
+		int[] w3 = new int[c1];
+		for(int x = 0; x<c1;x++) {
+			w3[x] = w1[x]+w2[x];
+			t= t+1;
+			bw.write("Case #"+t+": "+w3[x]+"\n");
+		}
+		bw.flush();
+		bw.close();
+	}
+
+}
