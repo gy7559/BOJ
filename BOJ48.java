b8e979670984becb6da901aa8bd55a580316e6a6
gy7559 <gy7559@naver.com>
2023-07-22 19:25:17 +0900

    Create BOJ48.java

---
+++ BOJ48.java
@@ -0,0 +1,45 @@
+package BOJ;
+import java.util.*;
+import java.io.*;
+public class BOJ48 {
+
+	public static void main(String[] args)throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		int sum =0;
+		String e = bf.readLine();
+		for(int a = 0; a<e.length();a++) {
+			char d = e.charAt(a);
+			switch(d) {
+			case 'A' : case'B': case'C':
+				sum += 3;
+				break;
+			case 'D' : case'E': case'F':
+				sum += 4;
+				break;
+			case 'G' : case'H': case'I':
+				sum += 5;
+				break;
+			case 'J' : case'K': case'L':
+				sum += 6;
+				break;
+			case 'N' : case'M': case'O':
+				sum += 7;
+				break;
+			case 'P' : case'Q': case'R': case'S':
+				sum += 8;
+				break;
+			case 'T' : case'U': case'V':
+				sum += 9;
+				break;
+			case 'W' : case'X': case'Y':case'Z':
+				sum += 10;
+				break;
+			}
+		
+		}
+		
+		System.out.println(sum);		
+		
+	}
+}
+
